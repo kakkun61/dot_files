1ef233367d6647a44b1b042a469ab08b6c44f66c
Kazuki Okamoto <okamoto-k@klab.com>
2014-06-13 20:31:57 +0900

    handle option

--- src/implicit-git-rm.rb
+++ src/implicit-git-rm.rb
@@ -2,10 +2,26 @@ def git_tracked? path
   system "git ls-files --error-unmatch #{path} >/dev/null 2>&1"
 end
 
+opts = []
+args = []
+endopt = false
 ARGV.each { |arg|
-  if git_tracked? arg then
-    `git rm #{arg}`
+  if endopt or arg[0] != '-' then
+    args.push arg
   else
-    `rm #{arg}`
+    opts.push arg
+    if arg == '--' then
+      endopt = true
+    end
+  end
+}
+
+optstr = opts.join ' '
+
+args.each { |arg|
+  if git_tracked? arg then
+    `git rm #{optstr} #{arg}`
+  else
+    `rm #{optstr} #{arg}`
   end
 }
