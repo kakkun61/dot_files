b9234b915d66d1dfdea1532dc07f0bd165df72d8
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2022-03-30 15:12:23 +0900

    show exit code

--- sh/lib.sh
+++ sh/lib.sh
@@ -37,9 +37,9 @@ setup_less() {
 setup_terminal() {
     if type __git_ps1 > /dev/null 2>&1
     then
-        PS1='\[\e]0;\w\a\]\n\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\]\[\e[36m\]$(__git_ps1 " %s")\[\e[0m\]\n\$ '
+        PS1='\[\e]0;\w\a\]\e[34m($?)\e[0m\n\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\]\[\e[36m\]$(__git_ps1 " %s")\[\e[0m\]\n\$ '
     else
-        PS1='\[\e]0;\w\a\]\n\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\]\n\$ '
+        PS1='\[\e]0;\w\a\]\e[34m($?)\e[0m\n\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\]\n\$ '
     fi
 }
 
