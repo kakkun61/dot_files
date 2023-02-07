2729ce69eae7e3a172bae9f0129172390834f41a
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2023-02-07 23:19:29 +0900

    PGP

--- home-manager/embed.bash
+++ home-manager/embed.bash
@@ -2,24 +2,35 @@
 
 set -e
 
+if [[ ! $# == 4 ]]
+then
+    echo "Usage $0 BASH_PROFILE_PATH BASH_INIT_PATH BASH_LOGOUT_PATH GPG_PUBLIC_PATH" >&2
+    exit 1
+fi
+
 while IFS= read -r line
 do
   case "$line" in
-    '      profileExtra = "";')
-      echo "      profileExtra ="
-      echo "        ''"
-      cat "$1"
-      echo "        '';";;
-    '      initExtra = "";')
-      echo "      initExtra ="
-      echo "        ''"
-      cat "$2"
-      echo "        '';";;
-    '      logoutExtra = "";')
-      echo "      logoutExtra ="
-      echo "        ''"
-      cat "$3"
-      echo "        '';";;
+    '    bash-profile = "";')
+        echo "    bash-profile ="
+        echo "        ''"
+        cat "$1"
+        echo "        '';";;
+    '    bash-init = "";')
+        echo "    bash-init ="
+        echo "        ''"
+        cat "$2"
+        echo "        '';";;
+    '    bash-logout = "";')
+        echo "    bash-logout ="
+        echo "        ''"
+        cat "$3"
+        echo "        '';";;
+    '    gpg-public-key = "";')
+        echo "    gpg-public-key ="
+        echo "        ''"
+        cat "$4"
+        echo "        '';";;
     *) echo "$line";;
   esac
 done
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,11 +1,18 @@
 { config, pkgs, ... }:
 
+let
+    bash-profile = "";
+    bash-init = "";
+    bash-logout = "";
+    gpg-public-key = "";
+in
 {
     home = {
         username = builtins.getEnv "USER";
         homeDirectory = builtins.getEnv "HOME";
         stateVersion = "22.11";
         packages = with pkgs; [
+            file
             git
             gnumake
             lesspipe
@@ -27,14 +34,24 @@
     programs = {
         bash = {
             enable = true;
-            profileExtra = "";
-            initExtra = "";
-            logoutExtra = "";
+            profileExtra = bash-profile;
+            initExtra = bash-init;
+            logoutExtra = bash-logout;
         };
         direnv = {
             enable = true;
             nix-direnv.enable = true;
         };
+        gpg = {
+            enable = true;
+            publicKeys = [ { text = gpg-public-key; } ];
+        };
         home-manager.enable = true;
+    };
+
+    services = {
+        gpg-agent = {
+            enable = true;
+        };
     };
 }
