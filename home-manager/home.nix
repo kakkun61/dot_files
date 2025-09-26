d1c41f52dc442f2b7c6a3212de202e5414a8cd68
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2025-09-26 14:07:49 +0900

    [home-manager] direnv のタイムアウト表示をしない

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -85,6 +85,9 @@
       enable = true;
       enableBashIntegration = true;
       nix-direnv.enable = true;
+      config = {
+        global.warn_timeout = 0;
+      };
     };
     gh = {
       enable = true;
