c17453260d2ba594424ad7ea745f146a7bac740f
Kazuki Okamoto (岡本和樹) <kazuki.okamoto@kakkun61.com>
2026-01-15 20:25:31 +0900

    バージョンを固定

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "This gives an environment to develop home.nix and provides it";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     envar = {
